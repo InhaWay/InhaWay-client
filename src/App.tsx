2423327efbd0bbf1604a3ea9e31d49cbd98e9062
huniversal <hunivers0523@naver.com>
2025-09-26 15:38:08 +0900

    Feat: 페이지별 보일러플레이트 세팅 및 라우팅 설정:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 function App() {
-  return <>안녕하세요</>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div>안녕하세요 메인페이지입니다.</div>;
+}
+
+export default index;
