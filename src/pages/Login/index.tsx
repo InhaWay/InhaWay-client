87f948ef1084162c47905d90db2154b191ac2587
huniversal <hunivers0523@naver.com>
2025-10-01 03:28:54 +0900

    Feat: 로그인 페이지 레이아웃 구현

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import kakaoLogin from "../../assets/kakaoLogin.svg";
+import noLogin from "../../assets/noLogin.svg";
+import character from "../../assets/character.svg";
+import { useNavigate } from "react-router-dom";
 
 function index() {
-  return <div>안녕하세요 로그인 페이지입니다.</div>;
+  const navvigate = useNavigate();
+
+  const handleNoLogin = () => {
+    navvigate("/");
+  };
+  return (
+    <div className="flex flex-col min-h-screen px-24 pt-60">
+      <div className="flex items-center justify-center flex-1">
+        <img src={character} alt="LoginCharacter" className="w-[300px]" />
+      </div>
+      <div className="flex flex-col gap-12 pb-40">
+        <img src={noLogin} onClick={handleNoLogin} alt="NoLoginBtn" className="w-full cursor-pointer" />
+        <img src={kakaoLogin} alt="KakaoBtn" className="w-full cursor-pointer" />
+      </div>
+    </div>
+  );
 }
 
 export default index;
