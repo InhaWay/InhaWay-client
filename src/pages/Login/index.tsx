6495e7d866e9c3aa54a1f34a29c3d80762f73264
huniversal <hunivers0523@naver.com>
2025-10-07 20:43:00 +0900

    Feat: 로그인 페이지 레이아웃 배치 완료

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,15 @@
-
-
 function index() {
-  return <div className='px-16 bg-p-white'>
-    <div className='flex flex-col items-center justify-center min-h-screen'>
-      <img src="/src/assets/Character.svg" alt="인하로운길 캐릭터" className='w-full'/>
+  return (
+    <div className="relative min-h-screen px-16 bg-p-white">
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <img src="/src/assets/Character.svg" alt="인하로운길 캐릭터" className="w-full" />
+      </div>
+      <div className="fixed flex flex-col gap-[10px] bottom-10 left-16 right-16">
+        <button className="py-3 text-white bg-primary-600 rounded-12">로그인 없이 간편 사용하기</button>
+        <button className="py-3 text-black bg-yellow-400 rounded-12">카카오 시작하기</button>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default index;
