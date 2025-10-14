1330a14f0451244f4562be3abcc1768121c378b5
huniversal <hunivers0523@naver.com>
2025-10-15 03:27:15 +0900

    Feat: Home index SearchBar 컴포넌트 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,16 @@
 import NaverMap from '../../shared/components/Map';
 import LocationSelector from '../../shared/components/LocationSelector';
+import SearchBar from '../../shared/components/SearchBar';
 
 function index() {
   return (
-    <div className="relative h-screen">
-      <NaverMap />
-      <div className="fixed bottom-0 left-0 right-0 z-[9999] bg-p-white rounded-t-24">
-        <LocationSelector />
+    <div>
+      <SearchBar />
+      <div className="relative h-screen">
+        <NaverMap />
+        <div className="fixed bottom-0 left-0 right-0 z-[9999] bg-p-white rounded-t-24">
+          <LocationSelector />
+        </div>
       </div>
     </div>
   );
