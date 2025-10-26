55fb3afb184e073710aeca6986618f874cfc2e8d
huniversal <hunivers0523@naver.com>
2025-10-26 16:36:49 +0900

    Feat: SearchBar 검색 btn

--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -7,13 +7,14 @@ interface SearchBarProps {
 const SearchBar = ({placeholder = '건물을 입력하세요'}: SearchBarProps) => {
   return (
     <div className='relative w-full px-16 py-12 bg-p-white'>
-      <div className='flex items-center gap-12 px-16 py-12 bg-gray-100 rounded-12'>
+      <div className='flex items-center gap-12 px-12 py-12 bg-gray-100 rounded-12'>
         <Search className='w-20 h-20 text-gray-400' />
         <input
           type='text'
           placeholder={placeholder}
           className='flex-1 text-gray-600 bg-gray-100 text-16'
         />
+        <button className='px-2 py-1 bg-primary-600 text-p-white rounded-8'>검색</button>
       </div>
     </div>
   )
