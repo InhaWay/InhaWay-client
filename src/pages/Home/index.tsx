ab727a043b00c0af4b1d565f4446a46c5411948f
huniversal <hunivers0523@naver.com>
2025-10-28 03:23:01 +0900

    Feat: 건물 데이터 상태관리

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,26 @@
-import NaverMap from '../../shared/components/Map';
-import LocationSelector from '../../shared/components/LocationSelector';
-import SearchBar from '../../shared/components/SearchBar';
+import { useState } from "react";
+import NaverMap from "../../shared/components/Map";
+import LocationSelector from "../../shared/components/LocationSelector";
+import SearchBar from "../../shared/components/SearchBar";
+
+interface Building {
+  _id: string;
+  name: string;
+  code: string;
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+}
 
 function index() {
+  const [buildings, setBuildings] = useState<Building[]>([]);
+  const [selectedBuilding, setSelectedBuilding] = useState<Building | null>(null);
   return (
     <div>
-      <SearchBar />
+      <SearchBar buildings={buildings} onSearch={setSelectedBuilding} />
       <div className="relative h-screen">
-        <NaverMap />
+        <NaverMap onBuildingsLoaded={setBuildings} selectedBuilding={selectedBuilding} />
         <div className="fixed bottom-0 left-0 right-0 z-[9999] bg-p-white rounded-t-24">
           <LocationSelector />
         </div>
